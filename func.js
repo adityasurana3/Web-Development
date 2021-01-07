2dbc52933e9b2ca968a644ad36d96192ba32e136
adityasurana3 <suranaaditya3@gmail.com>
2021-01-07 15:09:31 +0530

    first

---
+++ func.js
@@ -0,0 +1,11 @@
+function min(a,b) {
+    if (a>b){
+        console.log("A is greater")
+    }
+    else{
+        console.log("B is greater")
+    }
+}
+let x=10
+let y=20
+min(x,y);
---
+++ loops.js
@@ -0,0 +1,10 @@
+let car = ["Audi","BMW","Lambo"]
+// for(i=0;i<car.length;i++){
+//     console.log(car[i]);
+// }
+// car.forEach(function f(element)  {
+//     console.log(element)
+// });
+for (const item of car) {
+    console.log(item)
+}
